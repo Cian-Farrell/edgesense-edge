e98339e8830559de8f68307e72f7d5e379a4782f
Cian-Farrell <cian.farrell41@gmail.com>
2026-05-21 16:18:59 +0100

    Add dummy sensor Arduino sketch

---
+++ arduino/dummy_sensor.ino/dummy_sensor.ino.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("25.3,60.1");
+  delay(2000);
+}
